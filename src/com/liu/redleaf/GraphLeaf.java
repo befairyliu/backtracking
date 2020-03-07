3ad10472b5721831168df0cda243263f008d111a
befairyliu <befairyliu@gmail.com>
2020-03-07 21:29:00 +0800

    init

---
+++ src/com/liu/redleaf/GraphLeaf.java
@@ -0,0 +1,4 @@
+package com.liu.redleaf;
+
+public class GraphLeaf {
+}
---
+++ src/com/liu/redleaf/Main.java
@@ -0,0 +1,9 @@
+package com.liu.redleaf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello red leaf !");
+    }
+}
