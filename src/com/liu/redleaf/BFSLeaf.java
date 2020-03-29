81469bee8fbb7a8d78a3d3d98b7b539b5dcf1a3f
befariy.liu <befairyliu@gmail.com>
2020-03-29 22:04:54 +0800

    Create BFSLeaf.java

---
+++ src/com/liu/redleaf/BFSLeaf.java
@@ -0,0 +1,42 @@
+
+
+
+
+
+public int maxDistance(int[][] grid) {
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        Queue<int[]> queue = new ArrayDeque<>();
+        int n = grid.length;
+        // 先把所有的陆地都入队。
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    queue.offer(new int[] {i, j});
+                }
+            }
+        }
+        // 全是海洋或者全是陆地，返回-1。
+        if (queue.size() == 0 || queue.size() == n * n) {
+            return -1;
+        }
+
+        // 从各个陆地开始，一圈一圈的遍历海洋，最后遍历到的海洋就是离陆地最远的海洋。
+        int[] point = null;
+        while (!queue.isEmpty()) {
+            point = queue.poll();
+            int x = point[0], y = point[1];
+            // 取出队列的元素，将其四周的海洋入队。
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                if (newX < 0 || newX >= n || newY < 0 || newY >= n || grid[newX][newY] != 0) {
+                    continue;
+                }
+                grid[newX][newY] = grid[x][y] + 1; // 这里我直接修改了原数组，因此就不需要额外的数组来标志是否访问
+                queue.offer(new int[] {newX, newY});
+            }
+        }
+        // 返回最后一次遍历到的海洋的距离。
+        return grid[point[0]][point[1]] - 1;
+    }
