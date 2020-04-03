7f21221dffda092865b803031b428126b0d15c9f
befariy.liu <befairyliu@gmail.com>
2020-04-03 08:46:30 +0800

    Update BinaryTree.java

--- src/com/liu/vip207/BinaryTree.java
+++ src/com/liu/vip207/BinaryTree.java
@@ -3,5 +3,13 @@ package com.liu.vip207;
 import java.util.*;
 
 public class BinaryTree {
-
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    
+  
 }
