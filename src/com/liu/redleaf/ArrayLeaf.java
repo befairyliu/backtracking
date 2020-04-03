bd56f7d207efe4b5e39102b89f4261fa69e1e75e
befariy.liu <befairyliu@gmail.com>
2020-04-03 11:25:58 +0800

    Update ArrayLeaf.java

--- src/com/liu/redleaf/ArrayLeaf.java
+++ src/com/liu/redleaf/ArrayLeaf.java
@@ -582,7 +582,552 @@ public class ArrayLeaf {
         }
     }
   
-  
+    
+  //15. leetcode 面试题 17.16. 按摩师
+    //动态规划
+    public int massage(int[] nums){
+        int len = nums.length;
+        if(len == 0){
+            return 0;
+        }
+
+        if(len == 1){
+            return nums[0];
+        }
+
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i< len; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[len-1];
+    }
+
+    //16. leetcode 213. 打家劫舍 II
+    //动态规划
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+
+        //换成2次单排列
+        return Math.max(robHelper(Arrays.copyOfRange(nums, 0, nums.length - 1)),
+                robHelper(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
+
+    public static int robHelper(int[] nums){
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i=2; i< len; i++){
+            //状态转移方程
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[len-1];
+    }
+
+    //17. leetcode 892. 三维形体的表面积
+    public int surfaceArea(int[][] grid) {
+        //check params
+        if(grid == null ){
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        // 立方体总个数
+        int sum = 0;
+        // 垂直重叠个数
+        int verticalOverlap = 0;
+        // 行重叠个数
+        int rowOverlap = 0;
+        // 列重叠个数
+        int colOverlap = 0;
+
+        // 遍历二维数组
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum += grid[i][j];
+
+                if (grid[i][j] > 1) {
+                    verticalOverlap += (grid[i][j] - 1);
+                }
+
+                if (j > 0) {
+                    rowOverlap += Math.min(grid[i][j - 1], grid[i][j]);
+                }
+
+                if (i > 0) {
+                    colOverlap += Math.min(grid[i - 1][j], grid[i][j]);
+                }
+            }
+        }
+        return sum * 6 - (verticalOverlap + rowOverlap + colOverlap) * 2;
+    }
+
+    // 18. leetcode 999. 车的可用捕获量
+    public int numRookCaptures(char[][] board){
+        //定义方向数组
+        int[][] directions = {{-1,0}, {1,0},{0,1},{0,-1}};
+        //遍历
+        for(int i = 0; i < 8 ; i++){
+            for(int j = 0; j < 8; j++){
+                if(board[i][j] == 'R'){
+                    int res = 0;
+                    //上下左右四个方向遍历
+                    for(int[] direction : directions){
+                        if(burnout(board, i, j, direction)){
+                            res++;
+                        }
+                    }
+                    return res;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static boolean burnout(char[][] board, int x, int y, int[] direction){
+        int i = x;
+        int j = y;
+        //判断是否在棋盘范围内
+        while(inArea(i, j)){
+            // 找到友军
+            if(board[i][j] == 'B'){
+                break;
+            }
+            // 找到敌军
+            if(board[i][j] == 'p'){
+                return true;
+            }
+            // 找不到的时候该方向继续往下找
+            i += direction[0];
+            j += direction[1];
+        }
+
+        return false;
+    }
+
+    public static boolean inArea(int i, int j){
+        return i >= 0 && i < 8 && j >= 0 && j < 8;
+    }
+
+    //19. leetcode 面试题 08.10. 颜色填充
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int x = image.length;
+        int y = image[0].length;
+        if((sr < 0 || sr >= x) || (sc < 0 || sc >= y)){
+            return image;
+        }
+
+        int oldColor = image[sr][sc];
+        floodDFS(image, sr, sc, oldColor, newColor);
+        return image;
+    }
+
+    public static void floodDFS(int[][] image, int sr, int sc, int oldColor, int newColor){
+        // 检查条件
+        if(0 <= sr && sr < image.length
+                && 0 <= sc && sc < image[0].length
+                && image[sr][sc] == oldColor && oldColor != newColor){
+
+            //将找到的val标注标注为newColor
+            image[sr][sc] = newColor;
+            // 上下左右深度搜索遍历
+            floodDFS(image, sr, sc + 1, oldColor, newColor);
+            floodDFS(image, sr - 1, sc, oldColor, newColor);
+            floodDFS(image, sr + 1, sc, oldColor, newColor);
+            floodDFS(image, sr, sc - 1, oldColor, newColor);
+        }
+    }
+
+    //20. leetcode 820. 单词的压缩编码
+    // 单词后缀法
+    public int minimumLengthEncoding(String[] words){
+        //check params
+        if(words == null || words.length == 0){
+            return 0;
+        }
+
+        Set<String> set = new HashSet<>();
+        set.addAll(Arrays.asList(words));
+        for(String word: words){
+            for(int i = 1; i < word.length(); i++){
+                set.remove(word.substring(i));
+            }
+        }
+
+        int res = 0;
+        for(String word : set){
+            res = res + word.length() + 1;
+        }
+
+        return res;
+    }
+
+    // 字典树方法
+    // 字典树的叶子节点（没有孩子的节点）就代表没有后缀的单词，统计叶子节点代表的单词长度加一的和即为我们要的答案。
+    public int minimumLengthEncodingII(String[] words) {
+        TrieNode trie = new TrieNode();
+        Map<TrieNode, Integer> nodes = new HashMap();
+
+        for (int i = 0; i < words.length; ++i) {
+            String word = words[i];
+            TrieNode cur = trie;
+            for (int j = word.length() - 1; j >= 0; --j) {
+                cur = cur.build(word.charAt(j));
+            }
+            nodes.put(cur, i);
+        }
+
+        int ans = 0;
+        for (TrieNode node: nodes.keySet()) {
+            if (node.count == 0) {
+                ans += words[nodes.get(node)].length() + 1;
+            }
+        }
+        return ans;
+
+    }
+
+    class TrieNode {
+        TrieNode[] children;
+        int count;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            count = 0;
+        }
+
+        public TrieNode build(char c) {
+            if (children[c - 'a'] == null) {
+                children[c - 'a'] = new TrieNode();
+                count++;
+            }
+            return children[c - 'a'];
+        }
+    }
+
+    // 21. leetcode 914. 卡牌分组
+    public boolean hasGroupsSizeX(int[] deck){
+        int len = deck.length;
+        if(len < 2){
+            return false;
+        }
+
+        // 计数数组, 每个数字出现的次数
+        int[] cnt = new int[10000];
+        for(int num: deck){
+            cnt[num]++;
+        }
+
+        //init
+        int x = cnt[deck[0]];
+
+        for(int i = 0; i < 10000; i++){
+            if(cnt[i] == 1){
+                return false;
+            }
+
+            if(cnt[i] > 1){
+                x = gcd(x, cnt[i]);
+                //排除1
+                if(x == 1){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private int gcd(int a, int b){
+        if(b == 0){
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+
+
+    //22. leetcode 912. 排序数组, 升序排列
+    public int[] sortArray(int[] nums) {
+        return null;
+    }
+
+    // 23. leetcode 面试题57 - II. 和为s的连续正数序列
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> result = new ArrayList<>();
+        for(int i = 1; i <= target/2; i++){
+            int sum = 0;
+            int j = i;
+            while(sum < target){
+                sum += j;
+                j++;
+            }
+
+            if(sum == target){
+                int[] arr = new int[j - i];
+                for(int k = 0; k < j - i; k++){
+                    arr[k] = k + i;
+                }
+
+                result.add(arr);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    // 24. leetcode vpi -- 253 会议室II
+    // 方法一：优先队列
+    public int minMeetingRooms(int[][] intervals) {
+        int length = intervals.length;
+        Interval[] inters = new Interval[length];
+        for(int i = 0; i < length; i++){
+            inters[i] = new Interval(intervals[i][0], intervals[i][1]);
+        }
+        return roomsHelper(inters);
+    }
+
+    public static class Interval {
+        int start;
+        int end;
+
+        Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    // 1.按照 开始时间 对会议进行排序。
+    // 2.初始化一个新的 最小堆，将第一个会议的结束时间加入到堆中。我们只需要记录会议的结束时间，告诉我们什么时候房间会空。
+    // 3.对每个会议，检查堆的最小元素（即堆顶部的房间）是否空闲。
+    //  3.1若房间空闲，则从堆顶拿出该元素，将其改为我们处理的会议的结束时间，加回到堆中。
+    //  3.2若房间不空闲。开新房间，并加入到堆中。
+    // 4.处理完所有会议后，堆的大小即为开的房间数量。这就是容纳这些会议需要的最小房间数。
+
+    public int roomsHelper(Interval[] intervals) {
+        // Check for the base case. If there are no intervals, return 0
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        // Min heap
+        PriorityQueue<Integer> allocator =
+                new PriorityQueue<Integer>(
+                        intervals.length,
+                        new Comparator<Integer>() {
+                            public int compare(Integer a, Integer b) {
+                                return a - b;
+                            }
+                        });
+
+        // Sort the intervals by start time
+        Arrays.sort(intervals, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.start - o2.start;
+            }
+        });
+
+        // Add the first meeting
+        allocator.add(intervals[0].end);
+
+        // Iterate over remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
+            // If the room due to free up the earliest is free, assign that room to this meeting.
+            if (intervals[i].start >= allocator.peek()) {
+                allocator.poll();
+            }
+
+            // If a new room is to be assigned, then also we add to the heap,
+            // If an old room is allocated, then also we have to add to the heap with updated end time.
+            allocator.add(intervals[i].end);
+        }
+
+        // The size of the heap tells us the minimum rooms required for all the meetings.
+        return allocator.size();
+    }
+
+    // 方法二：有序化
+    // 1.分别将开始时间和结束时间存进两个数组。
+    // 2.分别对开始时间和结束时间进行排序。请注意，这将打乱开始时间和结束时间的原始对应关系。它们将被分别处理。
+    // 3.考虑两个指针： s_ptr 和 e_ptr，分别代表开始指针和结束指针。开始指针遍历每个会议，结束指针帮助我们跟踪会议是否结束。
+    // 4.当考虑 s_ptr 指向的特定会议时，检查该开始时间是否大于 e_ptr 指向的会议。若如此，则说明 s_ptr 开始时，已经有会议结束。于是我们可以重用房间。否则，我们就需要开新房间。
+    // 5.若有会议结束，换而言之， start[s_ptr] >= end[e_ptr]，则自增 e_ptr。
+    // 6.重复这一过程，直到 s_ptr 处理完所有会议
+    public int roomsHelperII(Interval[] intervals) {
+
+        // Check for the base case. If there are no intervals, return 0
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        Integer[] start = new Integer[intervals.length];
+        Integer[] end = new Integer[intervals.length];
+
+        for (int i = 0; i < intervals.length; i++) {
+            start[i] = intervals[i].start;
+            end[i] = intervals[i].end;
+        }
+
+        // Sort the intervals by end time
+        Arrays.sort( end, new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return a - b;
+                    }
+                });
+
+        // Sort the intervals by start time
+        Arrays.sort( start, new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return a - b;
+                    }
+                });
+
+        // The two pointers in the algorithm: e_ptr and s_ptr.
+        int startPointer = 0, endPointer = 0;
+
+        // Variables to keep track of maximum number of rooms used.
+        int usedRooms = 0;
+
+        // Iterate over intervals.
+        while (startPointer < intervals.length) {
+
+            // If there is a meeting that has ended by the time the meeting at `start_pointer` starts
+            if (start[startPointer] >= end[endPointer]) {
+                usedRooms -= 1;
+                endPointer += 1;
+            }
+
+            // We do this irrespective of whether a room frees up or not.
+            // If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
+            // remain the same in that case. If no room was free, then this would increase used_rooms
+            usedRooms += 1;
+            startPointer += 1;
+        }
+
+        return usedRooms;
+    }
+
+
+    // 25. leetcode 289. 生命游戏
+    // 方法一：复制原数组进行模拟，
+    // 时间复杂度：O(mn)，
+    // 空间复杂度：O(mn)   为复制数组占用的空间
+    public void gameOfLife(int[][] board) {
+        //相邻细胞的坐标位置向量
+        int[] neighbors = {-1, 0, 1};
+        int rows = board.length;
+        int cols = board[0].length;
+
+        //build copy board
+        int[][] copyBoard = new int[rows][cols];
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+
+        //iterate the cell of board
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                //对于每个细胞统计其八个相邻位置里的活细胞数量
+                int liveNeighbors = 0;
+                for(int i = 0; i < 3; i++){
+                    for(int j= 0; j < 3; j++){
+                        // neighbors[i] == 0 && neighbors[j] == 0的时候i == 0 && j == 0
+                        // 也就是细胞自己的位置向量
+                        if(!(neighbors[i] == 0 && neighbors[j] == 0)){
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // 查看相邻的细胞是否是活细胞
+                            if((r < rows && r >= 0) && (c < cols && c >= 0)
+                                    && (copyBoard[r][c] == 1)){
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // 规则1 或 规则2
+                if((copyBoard[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)){
+                    board[row][col] = 0;
+                }
+                //规则4
+                if(copyBoard[row][col] == 0 && liveNeighbors == 3){
+                    board[row][col] = 1;
+                }
+            }
+        }
+    }
+
+    //方法二：使用额外的状态
+    // 原数组细胞状态使用额外的状态存储
+    // 2： 0 -> 1 死细胞变成活细胞
+    // -1: 1 -> 0 活细胞变成死细胞
+    public void gameOfLifeII(int[][] board) {
+        //相邻细胞的坐标位置向量
+        int[] neighbors = {-1, 0, 1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // 遍历面板每一个格子里的细胞
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // 对于每一个细胞统计其八个相邻位置里的活细胞数量
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            // 相邻位置的坐标
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // 查看相邻的细胞是否是活细胞
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (Math.abs(board[r][c]) == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // 规则 1 或规则 3
+                if ((board[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    // -1 代表这个细胞过去是活的现在死了
+                    board[row][col] = -1;
+                }
+                // 规则 4
+                if (board[row][col] == 0 && liveNeighbors == 3) {
+                    // 2 代表这个细胞过去是死的现在活了
+                    board[row][col] = 2;
+                }
+            }
+        }
+
+        // 遍历 board 得到一次更新后的状态
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] > 0) {
+                    board[row][col] = 1;
+                } else {
+                    board[row][col] = 0;
+                }
+            }
+        }
+    }
+
 
     //===============================================
     public static final void main(String[] args){
